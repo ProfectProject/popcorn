8e5951c2eceb4898aa6e04afa754898072227bb9
hongjunpyo <ejhjjp11@naver.com>
2025-12-31 17:13:06 +0900

    fix : v12 테이블 명 수정 및 column 정리

--- backend/src/main/resources/db/migration/schema/V12__create_p_checkins_table.sql
+++ backend/src/main/resources/db/migration/schema/V12__create_p_checkins_table.sql
@@ -1,11 +1,8 @@
--- CHECK-IN
 CREATE TYPE qr_status AS ENUM ( 'UNUSED','USED' );
 
-CREATE TABLE p_order_qr_codes (
+CREATE TABLE p_qr_codes_checkin (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     order_id UUID NOT NULL REFERENCES p_orders(id),
     qr_code VARCHAR(255) NOT NULL UNIQUE,
-    status qr_status NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    created_by BIGINT REFERENCES p_users(id)
+    status qr_status NOT NULL
 );
