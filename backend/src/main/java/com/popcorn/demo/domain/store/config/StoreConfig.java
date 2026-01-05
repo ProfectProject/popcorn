58b2b48790f4609c53a2bddaac33bb96fc901537
newo <newo@newoui-MacBookPro.local>
2026-01-05 13:21:04 +0900

    test: store 도메인 테스트 환경 설정 및 테스트 데이터 추가 #4

--- backend/src/main/java/com/popcorn/demo/domain/store/config/StoreConfig.java
+++ backend/src/main/java/com/popcorn/demo/domain/store/config/StoreConfig.java
@@ -5,13 +5,11 @@ import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-@EnableJpaRepositories(basePackages = "com.popcorn.demo.domain.store.repository.jpa")
 @ComponentScan(basePackages = {
 		"com.popcorn.demo.domain.store.service",
 		"com.popcorn.demo.domain.store.repository",
