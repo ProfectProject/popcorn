46d73dde681091facdfbc4d3dba6af0ec2cc00bf
오채영 <ochaeyeong@ochaeyeong-ui-MacBookPro.local>
2026-01-06 18:39:51 +0900

    feat: 로그인 api swagger 테스트 구현(#56)

--- backend/src/main/java/com/popcorn/demo/domain/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/popcorn/demo/domain/auth/config/SecurityConfig.java
@@ -44,11 +44,12 @@ public class SecurityConfig {
 
         // ★ LoginFilter는 여기서 직접 생성 (Bean 등록 X)
         LoginFilter loginFilter = new LoginFilter(authManager, jwtUtil);
-        loginFilter.setFilterProcessesUrl("/api/v1/auth/login");
+        loginFilter.setFilterProcessesUrl("/api/auth/login");
 
         http.csrf().disable()
             .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/login").permitAll()
+                .requestMatchers("/api/auth/login").permitAll()
+                .requestMatchers("/api/v1/auth/login").permitAll() // swagger api 테스트
                 .requestMatchers("/api/v1/users/signup").permitAll()
                 .requestMatchers("/api/v1/users/**").hasAnyRole("CUSTOMER")
                 .anyRequest().authenticated()
--- backend/src/main/java/com/popcorn/demo/domain/users/service/UserService.java
+++ backend/src/main/java/com/popcorn/demo/domain/users/service/UserService.java
@@ -82,4 +82,23 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    /**
+     * 사용자 계정 탈퇴
+     */
+    @Transactional
+    public void deactivateUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        // 기존 이메일 가져오기
+        String originalEmail = user.getEmail();
+
+        // 개인정보 마스킹 : 이메일로
+        user.setEmail("deleted_" + userId + "_" + originalEmail);
+        user.setName("탈퇴회원"+"("+user.getName()+")");
+
+        user.setActive(false); // 비활성화
+
+        userRepository.save(user);
+    }
 }
