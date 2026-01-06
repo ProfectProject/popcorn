aced0c2d984931c41bf49fa7106634a84ceb22cc
오채영 <ochaeyeong@ochaeyeong-ui-MacBookPro.local>
2026-01-06 14:26:40 +0900

    feat: 회원/로그인api/권한부여 구현 #9 #23

--- backend/src/test/java/com/popcorn/demo/domain/user/service/UserServiceTest.java
+++ backend/src/test/java/com/popcorn/demo/domain/user/service/UserServiceTest.java
@@ -50,7 +50,7 @@ class UserServiceTest {
         request.setPasswordCheck("1234");
         request.setPhone("010-1234-5678");
         request.setName("test");
-        request.setRole(UserRole.USER);
+        request.setRole(UserRole.CUSTOMER);
 
         when(userRepository.findByEmail("test@example.com"))
                 .thenReturn(Optional.empty());
@@ -81,7 +81,7 @@ class UserServiceTest {
         assertNotNull(response);
         assertEquals("test@example.com", response.getEmail());
         assertEquals("test", response.getName());
-        assertEquals(UserRole.USER, response.getRole());
+        assertEquals(UserRole.CUSTOMER, response.getRole());
 
         //User 저장 검증
         verify(userRepository).save(any(User.class));
@@ -97,7 +97,7 @@ class UserServiceTest {
         request.setPasswordCheck("1234");
         request.setPhone("010-1234-5678");
         request.setName("test");
-        request.setRole(UserRole.USER);
+        request.setRole(UserRole.CUSTOMER);
 
         when(userRepository.findByEmail("test@example.com"))
                 .thenReturn(Optional.of(new User()));
