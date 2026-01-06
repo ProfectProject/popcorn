f6e1419d738344009185f58900eb61c868df51ed
오채영 <ochaeyeong@ochaeyeong-ui-MacBookPro.local>
2026-01-06 17:12:53 +0900

    feat: 내정보 조회, 수정(#52)

--- backend/src/main/java/com/popcorn/demo/domain/users/repository/UserRepository.java
+++ backend/src/main/java/com/popcorn/demo/domain/users/repository/UserRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email); //이메일로 사용자 조회
     Optional<User> findByEmailAndIsActiveTrue(String email); // isActive = true인 사용자만 조회
+    Optional<User> findByPhone(String phone); //전화번호로 사용자 조회
 
     User findByemail(String email);
 }
