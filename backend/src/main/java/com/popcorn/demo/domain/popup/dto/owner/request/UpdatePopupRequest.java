96847f3115d734ef3024ca55a6c7a58be9440aca
newo <newo@newoui-MacBookPro.local>
2026-01-07 17:15:13 +0900

    feat: Owner Popup 기본 정보 수정 및 상태 변경 (#66)

--- backend/src/main/java/com/popcorn/demo/domain/popup/dto/owner/request/UpdatePopupRequest.java
+++ backend/src/main/java/com/popcorn/demo/domain/popup/dto/owner/request/UpdatePopupRequest.java
@@ -1,4 +1,18 @@
 package com.popcorn.demo.domain.popup.dto.owner.request;
 
+
+import com.popcorn.demo.domain.popup.entity.enums.PopupCategory;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UpdatePopupRequest {
+
+    private String title;
+    private String description;
+    private PopupCategory popupCategory;
+
 }
