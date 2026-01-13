7c704f801f3d1645a6657d37f4f070f249bb433e
Roy <shuwj81@daum.net>
2026-01-13 19:05:15 +0900

    feat: test code 수정

--- backend/src/test/java/com/popcorn/demo/domain/order/service/OrderDomainServiceTest.java
+++ backend/src/test/java/com/popcorn/demo/domain/order/service/OrderDomainServiceTest.java
@@ -201,34 +201,6 @@ class OrderDomainServiceTest {
 	}
 
 	@Test
-	@DisplayName("주문 상태 변경 가능 검증 - ACCEPTED에서 RESERVED로 변경 가능")
-	void canChangeStatus_OwnerAcceptedToConfirmed_ReturnsTrue() {
-		// given
-		OrderStatus currentStatus = OrderStatus.ACCEPTED;
-		OrderStatus newStatus = OrderStatus.RESERVED;
-
-		// when
-		boolean canChange = orderDomainService.canChangeStatus(currentStatus, newStatus);
-
-		// then
-		assertThat(canChange).isTrue();
-	}
-
-	@Test
-	@DisplayName("주문 상태 변경 가능 검증 - RESERVED에서 PAYMENT_PENDING으로 변경 가능")
-	void canChangeStatus_ConfirmedToPreparing_ReturnsTrue() {
-		// given
-		OrderStatus currentStatus = OrderStatus.RESERVED;
-		OrderStatus newStatus = OrderStatus.PAYMENT_PENDING;
-
-		// when
-		boolean canChange = orderDomainService.canChangeStatus(currentStatus, newStatus);
-
-		// then
-		assertThat(canChange).isTrue();
-	}
-
-	@Test
 	@DisplayName("주문 상태 변경 가능 검증 - REQUESTED에서 PAYMENT_PENDING로 변경 가능")
 	void canChangeStatus_RequestedToPending_ReturnsTrue() {
 		// given
@@ -268,20 +240,6 @@ class OrderDomainServiceTest {
 
 		// then
 		assertThat(canChange).isTrue();
-	}
-
-	@Test
-	@DisplayName("주문 상태 변경 가능 검증 - REQUESTED에서 PAYMENT_PENDING로 변경 불가")
-	void canChangeStatus_RequestedToReady_ReturnsFalse() {
-		// given
-		OrderStatus currentStatus = OrderStatus.REQUESTED;
-		OrderStatus newStatus = OrderStatus.PAYMENT_PENDING;
-
-		// when
-		boolean canChange = orderDomainService.canChangeStatus(currentStatus, newStatus);
-
-		// then
-		assertThat(canChange).isFalse();
 	}
 
 	@Test
