4f048a9fdfefa97f3e6ee6c0fc7db3034c2e3263
Roy <shuwj81@daum.net>
2026-01-16 01:10:07 +0900

    feat:테스트 코드 수정

--- backend/src/test/java/com/popcorn/demo/config/TestSecurityConfig.java
+++ backend/src/test/java/com/popcorn/demo/config/TestSecurityConfig.java
@@ -1,8 +1,8 @@
 package com.popcorn.demo.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,12 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile({"test", "!security-test"}) // test 프로필에서 활성화, SecurityConfigTest에서는 비활성화
+@Profile("test & !security-test") // test 프로필이면서 security-test 프로필이 아닐 때만 활성화
 public class TestSecurityConfig {
 
     @Bean
-    @Primary
-    @Order(1)
+    @Order(2) // SecurityConfig보다 낮은 우선순위
     public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
         http
             .securityMatcher("/api/**")  // 특정 경로만 처리하도록 제한
