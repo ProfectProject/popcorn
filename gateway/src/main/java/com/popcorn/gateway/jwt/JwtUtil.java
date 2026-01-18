76d7605cce9d330e0d09682f98614c363c91e41a
오채영 <ochaeyeong@ochaeyeong-ui-MacBookPro.local>
2026-01-18 20:27:01 +0900

    feat: gateway jwt 인증 필터 구현 (#105)

---
+++ gateway/src/main/java/com/popcorn/gateway/jwt/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.popcorn.gateway.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+
+
+// jwt 검증 담당 클래스
+
+@Component
+public class JwtUtil {
+
+    //private Key key;
+
+    private final SecretKey secretKey;
+    public JwtUtil(@Value("${jwt.secret}")String secret) {
+
+		byte[] byteSecretKey = Decoders.BASE64.decode(secret);
+        secretKey = Keys.hmacShaKeyFor(byteSecretKey);
+    }
+
+    public Long getUserId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);  // Long 타입으로 꺼내기
+    }
+
+    public String getUsername(String token) {
+
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().get("email", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
+
